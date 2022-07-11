70b461c649e101fd4122915e2bfb789916668227
erfanfi79 <erfanfaravani@gmail.com>
2022-07-11 22:35:48 +0430

    Merge with master

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.REACT_APP_BASENAME || ""}>
+  <BrowserRouter basename={process.env.PUBLIC_URL || ""}>
     <CookiesProvider>
       <App />
    </CookiesProvider>

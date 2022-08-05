6525d34a3c18f5befd90397cdb347e77092b23a0
erfanfi79 <erfanfaravani@gmail.com>
2022-08-05 13:56:44 +0430

    Fix Feedback Ui

--- src/components/feedbacks.js
+++ src/components/feedbacks.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { ListGroup } from "react-bootstrap";
+import { Button, ListGroup, Modal, Row, Table } from "react-bootstrap";
 
 import { css, StyleSheet } from "aphrodite";
 import { toast } from "react-toastify";
@@ -25,41 +25,97 @@ const FeedbackPanel = ({}) => {
   }, [error, data]);
 
   return (
-    <div className={css(styles.notificationList)}>
-      <ListGroup>
-        <ListGroup.Item>
-          <ListGroup horizontal className={css(styles.notificationItem)}>
-            <ListGroup.Item style={{ border: "0px" }}>موضوع</ListGroup.Item>
-            <ListGroup.Item style={{ border: "0px" }}>فرستنده</ListGroup.Item>
-            <ListGroup.Item style={{ border: "0px" }}>توضیحات</ListGroup.Item>
-            <ListGroup.Item style={{ border: "0px" }}>تاریخ</ListGroup.Item>
-          </ListGroup>
-        </ListGroup.Item>
-        <br />
-
-        {notifs &&
-          notifs.map((notif) => (
-            <ListGroup.Item>
-              <ListGroup horizontal className={css(styles.notificationItem)}>
-                <ListGroup.Item style={{ border: "0px" }}>
-                  {notif.title}
-                </ListGroup.Item>{" "}
-                <ListGroup.Item style={{ border: "0px" }}>
-                  {notif.writerEmail}
-                </ListGroup.Item>
-                <ListGroup.Item style={{ border: "0px" }}>
-                  {notif.content}
-                </ListGroup.Item>
-                <ListGroup.Item style={{ border: "0px" }}>
-                  {notif.timeStamp.slice(0,10)}
-                </ListGroup.Item>
-              </ListGroup>
-            </ListGroup.Item>
-          ))}
-      </ListGroup>
+    <div dir="rtl">
+      <h2 className="text-center" style={{ padding: "1em" }}>
+        فهرست پیشنهادات و انتقادات
+      </h2>
+      <Row>
+        <Table striped bordered responsive hover>
+          <thead>
+            <tr>
+              <th>موضوع</th>
+              <th>فرستنده</th>
+              <th>توضیحات</th>
+              <th>تاریخ</th>
+            </tr>
+          </thead>
+          <tbody>
+            {notifs &&
+              notifs.map((notif) => (
+                <tr key={notif.id}>
+                  <td> {notif.title} </td>
+                  <td> {notif.writerEmail} </td>
+                  <td>
+                    <DescriptionModal description={notif.content}
+                     /> 
+                  </td>
+                  <td dir="ltr"> {notif.timeStamp.slice(0, 10)} </td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Row>
     </div>
+    // <div className={css(styles.notificationList)}>
+    //   <ListGroup>
+    //     <ListGroup.Item>
+    //       <ListGroup horizontal className={css(styles.notificationItem)}>
+    //         <ListGroup.Item style={{ border: "0px" }}>موضوع</ListGroup.Item>
+    //         <ListGroup.Item style={{ border: "0px" }}>فرستنده</ListGroup.Item>
+    //         <ListGroup.Item style={{ border: "0px" }}>توضیحات</ListGroup.Item>
+    //         <ListGroup.Item style={{ border: "0px" }}>تاریخ</ListGroup.Item>
+    //       </ListGroup>
+    //     </ListGroup.Item>
+    //     <br />
+
+    //     {notifs &&
+    //       notifs.map((notif) => (
+    //         <ListGroup.Item>
+    //           <ListGroup horizontal className={css(styles.notificationItem)}>
+    //             <ListGroup.Item style={{ border: "0px" }}>
+    //               {notif.title}
+    //             </ListGroup.Item>{" "}
+    //             <ListGroup.Item style={{ border: "0px" }}>
+    //               {notif.writerEmail}
+    //             </ListGroup.Item>
+    //             <ListGroup.Item style={{ border: "0px" }}>
+    //               {notif.content}
+    //             </ListGroup.Item>
+    //             <ListGroup.Item style={{ border: "0px" }}>
+    //               {notif.timeStamp.slice(0,10)}
+    //             </ListGroup.Item>
+    //           </ListGroup>
+    //         </ListGroup.Item>
+    //       ))}
+    //   </ListGroup>
+    // </div>
   );
 };
+
+function DescriptionModal({description}) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+      {"مشاهده توضیحات"}
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+
+        <Modal.Body>{description}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            بستن
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 const styles = StyleSheet.create({
   notificationList: {
     direction: "rtl",
